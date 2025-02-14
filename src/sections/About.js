5c7f4c7e68c77c86d50f290d0d4c0a9be776031f
Inumoh Temi <temiloluwainumoh@gmail.com>
2025-02-14 04:49:32 +0100

    Updated the index.html, About.js, TechStack.js, and installed some packages

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,10 +1,11 @@
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SchoolIcon from '@mui/icons-material/School';
 
 export default function About() {
   return (
-    <div className="min-h-screen flex flex-col gap-9 px-4 md:px-[120px] py-[9vh] md:py-[14vh]">
+    <div className="min-h-screen flex flex-col gap-7 px-4 md:px-[120px] py-[9vh] md:py-[14vh]">
       <h1 className="text-black font-poppins font-bold md:text-[36px] leading-[52px]"
         style={{letterSpacing: '-0.4px'}}
       >About Me
@@ -12,38 +13,202 @@ export default function About() {
       <p className="text-gray-700 font-normal font-poppins text-[18px] leading-[26px]">
         Hi, I'm Inumoh Temiloluwa Donald, a passionate web developer with five years of experience in crafting digital experiences. My journey into web development began in Grade 12, when a friend from church introduced me to the world of building websites. Fascinated by the endless possibilities, I started learning HTML and CSS, practicing relentlessly and creating my own projects. <br />
         By my second year in university, I had already mastered responsive design and was diving deep into React to build dynamic, user-friendly applications. To refine my skills, I invested in a Udemy course, expanding my knowledge and working on even more projects.<br/>
-        Today, I specialize in creating seamless, modern web applications that blend aesthetics with functionality.
+        Today, I specialize in creating seamless, modern web applications that blend aesthetics with functionality. When I'm not coding, catch me trading memecoins or playing video games.
       </p>
 
       <h1 className="text-black font-poppins font-bold md:text-[36px] leading-[52px]"
-      style={{letterSpacing: '-0.4px'}}>
+        style={{letterSpacing: '-0.4px'}}>
         Work Experience
       </h1>
       
-      <div className='flex'>
-        <div>
-          <h1>Donor Care Representative</h1>
-          <div>
-            <div>
-              <ApartmentIcon />
-              <p>Siloam Mission</p>
-            </div>
-
-            <div>
-              <LocationOnIcon />
-              <p>Canada</p>
-            </div>
-          </div>
+      <div className='flex flex-col w-full'>
+        <div className="flex flex-row justify-between">
+          <h1 className="font-poppins font-normal text-[#666666] text-[20px] leading-[28px]"
+            style={{letterSpacing: "1px"}}
+          >
+            Donor Care Representative
+          </h1>
+          <p className="flex justify-center align-center px-[20px] py-[5px] bg-[#D7FFE0] text-[#018C0F] text-[9px] font-poppins font-semibold leading-[26px] tracking-wide rounded-full"
+            style={{letterSpacing: "1px"}}
+          >
+            Seasonal
+          </p> 
         </div>
 
-        <div>
-          <div>Seasonal</div>
-          <div>
-            <CalendarMonthIcon />
-            <p>Oct 2024 - Feb 2025</p>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-[20px]">
+            <div className="flex flex-row gap-1">
+              <ApartmentIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]' 
+                style={{letterSpacing: "1px"}}
+              >
+                Siloam Mission
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-1">
+              <LocationOnIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+                style={{letterSpacing: "0.5px"}}
+              >
+                Canada
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-1">
+            <CalendarMonthIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+            <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+              style={{letterSpacing: "1px"}}
+            >
+              Oct 2024 - Feb 2025
+            </p>
           </div>
         </div>
       </div>
+      <hr className='w-full h-[2px] border-gray-400'/>
+
+      <div className='flex flex-col w-full'>
+        <div className="flex flex-row justify-between">
+          <h1 className="font-poppins font-normal text-[#666666] text-[20px] leading-[28px]"
+            style={{letterSpacing: "1px"}}
+          >
+            Web Developer
+          </h1>
+          <p className="flex justify-center align-center px-[20px] py-[5px] bg-[#D7FFE0] text-[#018C0F] text-[9px] font-poppins font-semibold leading-[26px] tracking-wide rounded-full"
+            style={{letterSpacing: "1px"}}
+          >
+            Internship
+          </p> 
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-[20px]">
+            <div className="flex flex-row gap-1">
+              <ApartmentIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]' 
+                style={{letterSpacing: "1px"}}
+              >
+                The Breej Global Ltd.
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-1">
+              <LocationOnIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+                style={{letterSpacing: "0.5px"}}
+              >
+                Nigeria
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-1">
+            <CalendarMonthIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+            <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+              style={{letterSpacing: "1px"}}
+            >
+              June 2023 - Sept 2023
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className='w-full h-[3px] border-gray-400'/>
+
+      <div className='flex flex-col w-full'>
+        <div className="flex flex-row justify-between">
+          <h1 className="font-poppins font-normal text-[#666666] text-[20px] leading-[28px]"
+            style={{letterSpacing: "1px"}}
+          >
+            Junior Web Developer
+          </h1>
+          <p className="flex justify-center align-center px-[20px] py-[5px] bg-[#D7FFE0] text-[#018C0F] text-[9px] font-poppins font-semibold leading-[26px] tracking-wide rounded-full"
+            style={{letterSpacing: "1px"}}
+          >
+            Full Time
+          </p> 
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-[20px]">
+            <div className="flex flex-row gap-1">
+              <ApartmentIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]' 
+                style={{letterSpacing: "1px"}}
+              >
+                Buuka Technologies
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-1">
+              <LocationOnIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+                style={{letterSpacing: "0.5px"}}
+              >
+                Nigeria
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-1">
+            <CalendarMonthIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+            <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+              style={{letterSpacing: "1px"}}
+            >
+              Aug 2022 - Nov 2022
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className='w-full h-[3px] border-gray-400'/>
+
+      <h1 className="text-black font-poppins font-bold md:text-[36px] leading-[52px]"
+        style={{letterSpacing: '-0.4px'}}>
+        Education
+      </h1>
+      
+      <div className='flex flex-col w-full'>
+        <div className="flex flex-row justify-between">
+          <h1 className="font-poppins font-normal text-[#666666] text-[20px] leading-[28px]"
+            style={{letterSpacing: "1px"}}
+          >
+            BSc. in Computer Science
+          </h1>
+          <p className="flex justify-center align-center px-[20px] py-[5px] bg-[#D7FFE0] text-[#018C0F] text-[9px] font-poppins font-semibold leading-[26px] tracking-wide rounded-full"
+            style={{letterSpacing: "1px"}}
+          >
+            Full Time
+          </p> 
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-[20px]">
+            <div className="flex flex-row gap-1">
+              <SchoolIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]' 
+                style={{letterSpacing: "1px"}}
+              >
+                Redeemer's University
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-1">
+              <LocationOnIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+              <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+                style={{letterSpacing: "0.5px"}}
+              >
+                Nigeria
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-1">
+            <CalendarMonthIcon className="text-[#A7A7A7] w-[9px] h-[9px]"/>
+            <p className='font-poppins font-medium text-[12px] leading-[28px] text-[#A7A7A7]'
+              style={{letterSpacing: "1px"}}
+            >
+              Nov 2020 - Oct 2024
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className='w-full h-[2px] border-gray-400'/>
     </div>
   );
 }
--- src/sections/TechStack.js
+++ src/sections/TechStack.js
@@ -1,7 +1,65 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 export default function TechStack() {
   return (
-    <div className="min-h-screen bg-red-200 flex items-center justify-center">
-      <h1 className="text-4xl">Tech Stack Section</h1>
+    <div className="min-h-screen flex flex-col px-4 md:px-[120px] py-[9vh] md:py-[14vh]">
+      <h1 className="text-black font-poppins font-bold md:text-[36px] leading-[52px] mb-5"
+        style={{letterSpacing: '-0.4px'}}
+      >
+        Tech Stack
+      </h1>
+      <p className="text-gray-700 font-normal font-poppins text-[18px] leading-[26px] mb-[60px]">Technologies I've been working with recently</p>
+
+      <div className="grid grid-cols-5 gap-[40px] justify-center items-center">
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+        <div className="group w-full h-[186px] rounded border-2 border-black flex flex-col items-center justify-center gap-[32px] transition-all duration-300 hover:bg-black">
+          <GitHubIcon className="w-11 h-11 text-black group-hover:text-white transition-all duration-300" />
+          <p className="text-black font-poppins font-bold text-[20px] leading-[24%] group-hover:text-white transition-all duration-300"
+            style={{ letterSpacing: "-2%" }}
+          >
+            Git
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
