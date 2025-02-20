c9f335f88880bde92b4d058d83486ca527760bb9
Inumoh Temi <temiloluwainumoh@gmail.com>
2025-02-20 21:59:05 +0100

    Updated Project.js

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -13,46 +13,58 @@ export default function Projects() {
       </h1>
       <p className="text-gray-700 font-normal font-poppins text-[18px] leading-[26px] mb-[60px]">A few things I've built so far</p>
       <div className='flex flex-col gap-10'>
-        <div className='flex flex-row justify-between items-end'>
-          <img src="/images/icon-git.png" alt="" className=' h-[400px]'/>
-          <div className='flex flex-col gap-[28px]'>
-            <h1 className='font-poppins font-extrabold text-[48px] text-black leading-[56px]'
-              style={{letterSpacing: "-2%"}}
+        <div className="flex flex-row justify-between w-full h-[300px]">
+          {/* Image */}
+          <img src="/images/icon-git.png" alt="" className="h-[300px]" />
+
+          {/* Content */}
+          <div className="flex flex-col gap-[20px] max-w-[600px]">
+            <h1
+              className="font-poppins font-extrabold text-[48px] text-black leading-[56px]"
+              style={{ letterSpacing: "-2%" }}
             >
               01
             </h1>
-            <h2 className='font-poppins font-bold text-[32px] text-black leading-[40px]'
-              style={{letterSpacing: "-2%"}}
+            <h2
+              className="font-poppins font-bold text-[32px] text-black leading-[40px]"
+              style={{ letterSpacing: "-2%" }}
             >
               E-commerce website
             </h2>
-            <div className='flex flex-col gap-3'>
-              <p className='font-poppins font-normal text-[16px] leading-[24px]'
-              style={{letterSpacing: "2%"}}
+            <div className="flex flex-col gap-3">
+              <p
+                className="font-poppins font-normal text-[16px] leading-[24px]"
+                style={{ letterSpacing: "2%" }}
               >
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi iste voluptates.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in
+                voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore
+                reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi
+                iste voluptates.
               </p>
-              <p className='font-poppins font-normal text-[14px] leading-[26px]'><span className='font-semibold'>Tech Stack:</span> HTML, JavaScript, CSS</p>
+              <p className="font-poppins font-normal text-[14px] leading-[26px]">
+                <span className="font-semibold">Tech Stack:</span> HTML, JavaScript, CSS
+              </p>
             </div>
-            
-            <div className='flex flex-row gap-12'>
-              <div className='flex gap-[10px]'>
+
+            {/* Links */}
+            <div className="flex flex-row gap-12">
+              <div className="flex gap-[10px]">
                 <OpenInNewIcon />
-                <a 
-                  href={project1Url} 
-                  target='_blank' 
-                  rel='noopener noreferrer'
+                <a
+                  href={project1Url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
                   Live Preview
                 </a>
               </div>
 
-              <div className='flex gap-[10px]'>
+              <div className="flex gap-[10px]">
                 <GitHubIcon />
-                <a 
-                  href={project1Github} 
-                  target="_blank" 
+                <a
+                  href={project1Github}
+                  target="_blank"
                   rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
@@ -62,46 +74,56 @@ export default function Projects() {
             </div>
           </div>
         </div>
-
-        <div className='flex flex-row justify-between items-end'>
-          <div className='flex flex-col gap-[28px]'>
-            <h1 className='font-poppins font-extrabold text-[48px] text-black leading-[56px]'
-              style={{letterSpacing: "-2%"}}
+        
+        <div className="flex flex-row justify-between w-full h-[300px]">
+          {/* Content */}
+          <div className="flex flex-col gap-[20px] max-w-[600px]">
+            <h1
+              className="font-poppins font-extrabold text-[48px] text-black leading-[56px]"
+              style={{ letterSpacing: "-2%" }}
             >
               01
             </h1>
-            <h2 className='font-poppins font-bold text-[32px] text-black leading-[40px]'
-              style={{letterSpacing: "-2%"}}
+            <h2
+              className="font-poppins font-bold text-[32px] text-black leading-[40px]"
+              style={{ letterSpacing: "-2%" }}
             >
               E-commerce website
             </h2>
-            <div className='flex flex-col gap-3'>
-              <p className='font-poppins font-normal text-[16px] leading-[24px]'
-              style={{letterSpacing: "2%"}}
+            <div className="flex flex-col gap-3">
+              <p
+                className="font-poppins font-normal text-[16px] leading-[24px]"
+                style={{ letterSpacing: "2%" }}
               >
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi iste voluptates.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in
+                voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore
+                reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi
+                iste voluptates.
               </p>
-              <p className='font-poppins font-normal text-[14px] leading-[26px]'><span className='font-semibold'>Tech Stack:</span> HTML, JavaScript, CSS</p>
+              <p className="font-poppins font-normal text-[14px] leading-[26px]">
+                <span className="font-semibold">Tech Stack:</span> HTML, JavaScript, CSS
+              </p>
             </div>
-            
-            <div className='flex flex-row gap-12'>
-              <div className='flex gap-[10px]'>
+
+            {/* Links */}
+            <div className="flex flex-row gap-12">
+              <div className="flex gap-[10px]">
                 <OpenInNewIcon />
-                <a 
-                  href={project1Url} 
-                  target='_blank' 
-                  rel='noopener noreferrer'
+                <a
+                  href={project1Url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
                   Live Preview
                 </a>
               </div>
 
-              <div className='flex gap-[10px]'>
+              <div className="flex gap-[10px]">
                 <GitHubIcon />
-                <a 
-                  href={project1Github} 
-                  target="_blank" 
+                <a
+                  href={project1Github}
+                  target="_blank"
                   rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
@@ -110,49 +132,62 @@ export default function Projects() {
               </div>
             </div>
           </div>
-          <img src="/images/icon-git.png" alt="" className=' h-[400px]'/>
+          {/* Image */}
+          <img src="/images/icon-git.png" alt="" className="h-[300px]" />
         </div>
 
-        <div className='flex flex-row justify-between items-end'>
-          <img src="/images/icon-git.png" alt="" className=' h-[400px]'/>
-          <div className='flex flex-col gap-[28px]'>
-            <h1 className='font-poppins font-extrabold text-[48px] text-black leading-[56px]'
-              style={{letterSpacing: "-2%"}}
+        <div className="flex flex-row justify-between w-full h-[300px]">
+          {/* Image */}
+          <img src="/images/icon-git.png" alt="" className="h-[300px]" />
+
+          {/* Content */}
+          <div className="flex flex-col gap-[20px] max-w-[600px]">
+            <h1
+              className="font-poppins font-extrabold text-[48px] text-black leading-[56px]"
+              style={{ letterSpacing: "-2%" }}
             >
               01
             </h1>
-            <h2 className='font-poppins font-bold text-[32px] text-black leading-[40px]'
-              style={{letterSpacing: "-2%"}}
+            <h2
+              className="font-poppins font-bold text-[32px] text-black leading-[40px]"
+              style={{ letterSpacing: "-2%" }}
             >
               E-commerce website
             </h2>
-            <div className='flex flex-col gap-3'>
-              <p className='font-poppins font-normal text-[16px] leading-[24px]'
-              style={{letterSpacing: "2%"}}
+            <div className="flex flex-col gap-3">
+              <p
+                className="font-poppins font-normal text-[16px] leading-[24px]"
+                style={{ letterSpacing: "2%" }}
               >
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi iste voluptates.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem in
+                voluptatum, ipsum repellendus earum ducimus, numquam sit magnam, labore
+                reiciendis exercitationem veritatis explicabo. Earum aut ex ut, sequi
+                iste voluptates.
               </p>
-              <p className='font-poppins font-normal text-[14px] leading-[26px]'><span className='font-semibold'>Tech Stack:</span> HTML, JavaScript, CSS</p>
+              <p className="font-poppins font-normal text-[14px] leading-[26px]">
+                <span className="font-semibold">Tech Stack:</span> HTML, JavaScript, CSS
+              </p>
             </div>
-            
-            <div className='flex flex-row gap-12'>
-              <div className='flex gap-[10px]'>
+
+            {/* Links */}
+            <div className="flex flex-row gap-12">
+              <div className="flex gap-[10px]">
                 <OpenInNewIcon />
-                <a 
-                  href={project1Url} 
-                  target='_blank' 
-                  rel='noopener noreferrer'
+                <a
+                  href={project1Url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
                   Live Preview
                 </a>
               </div>
 
-              <div className='flex gap-[10px]'>
+              <div className="flex gap-[10px]">
                 <GitHubIcon />
-                <a 
-                  href={project1Github} 
-                  target="_blank" 
+                <a
+                  href={project1Github}
+                  target="_blank"
                   rel="noopener noreferrer"
                   className="font-poppins font-normal text-[16px] leading-[26px] text-black hover:underline"
                 >
