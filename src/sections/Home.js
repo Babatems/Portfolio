3288a552eeecfa693e482db3f0b8b1e46a9e8024
Inumoh Temi <temiloluwainumoh@gmail.com>
2025-06-16 17:09:26 -0500

    Updated

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Typewriter from '../sections/Typewriter';
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(false);
@@ -16,10 +17,33 @@ export default function Home() {
     >
       <div className="flex flex-col md:flex-row justify-between items-center md:items-end w-full">
         <h1
-          className="font-sans font-medium text-black text-[40px] md:text-[80px] leading-[110%] md:leading-[97.2%] mb-4 text-center md:text-left"
-          style={{ letterSpacing: '-3%' }}
+      className="font-sans font-medium text-black text-[40px] md:text-[80px] leading-[110%] md:leading-[97.2%] mb-4 text-center md:text-left"
+      style={{ letterSpacing: '-3%' }}
         >
-          Hello, I'm <br /> Inumoh Temiloluwa
+          <span className="inline-block mr-2">
+            <Typewriter
+              text={[
+                "Hello",         // English
+                "Bonjour",       // French
+                "Hola",          // Spanish
+                "Ciao",          // Italian
+                "Olá",           // Portuguese
+                "Hallo",         // German
+                "こんにちは",    // Japanese (Konnichiwa)
+                "안녕하세요",     // Korean (Annyeonghaseyo)
+                "مرحبا",         // Arabic (Marhaban)
+                "नमस्ते",         // Hindi (Namaste)
+                "你好",           // Chinese (Nǐ hǎo)
+              ]}
+              speed={100}
+              waitTime={1500}
+              deleteSpeed={40}
+              cursorChar={"_"}
+              className="text-yellow-600"
+            />
+          </span>
+          I'm <br />
+          Inumoh Temiloluwa
         </h1>
         <p
           className="font-sans font-normal text-[18px] md:text-[25px] text-gray-700 leading-[140%] md:leading-[117%] mb-4 text-center md:text-left"
